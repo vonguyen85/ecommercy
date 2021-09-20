7ca2c947b1744149e9e33ee74264fb5f8dfecd0c
vonguyen85 <90881767+vonguyen85@users.noreply.github.com>
2021-09-20 22:32:20 +0700

    get API products

--- client/src/GlobalState.js
+++ client/src/GlobalState.js
@@ -1,10 +1,19 @@
 import React, {createContext, useState} from 'react'
+import ProductsAPI from './api/ProductsAPI'
+
 
 export const GlobalState = createContext()
 
 export const DataProvider = ({children}) =>{
+    const [token, setToken] = useState(false)
+
+    const state = {
+        token: [token, setToken],
+        ProductsAPI: ProductsAPI()
+    }
+
     return (
-        <GlobalState.Provider value={{name: 'aa', date: '11/11'}}>
+        <GlobalState.Provider value={state}>
             {children}
         </GlobalState.Provider>
     )
--- client/src/components/mainpages/products/Products.js
+++ client/src/components/mainpages/products/Products.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalState } from '../../../GlobalState'
+
 
 export default function Products() {
+    const state = useContext(GlobalState)
+    const [products] = state.ProductsAPI.products
+    console.log(products)
     return (
         <div>
             Product
